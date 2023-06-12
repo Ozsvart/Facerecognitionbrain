b8cff6bb2af5a7ca0cbb65ff30259d3c9f5753d1
OViki <v.ozsvart@gmail.com>
2023-06-12 22:44:58 +0200

    Creating a logo

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React, {Component } from 'react';
 import Navigation from './Components/Navigation/Navigation';
+import Logo from './Components/Logo/Logo'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Navigation />
-      {/*<Logo />
-        <ImageLinkForm />
+      <Logo />
+        {/*<ImageLinkForm />
         <Facerecognition />*/}
     </div>
   );
