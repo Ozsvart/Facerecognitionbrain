f5517aaa56f89b2ee837e9f9b1041ade9aa99cba
OViki <v.ozsvart@gmail.com>
2023-06-23 23:03:14 +0200

    Rank Component, Particles

--- src/App.js
+++ src/App.js
@@ -1,14 +1,32 @@
 import React, {Component } from 'react';
+import Particles from 'react-particles';
 import Navigation from './Components/Navigation/Navigation';
 import Logo from './Components/Logo/Logo';
 import ImageLinkForm from './Components/ImageLinkForm/ImageLinkForm';
+import Rank from './Components/Rank/Rank';
 import './App.css';
+
+const particleOptions = {
+  particles: {
+    number: {
+      value: 30,
+      density: {
+        enable: true,
+        value_area: 800,
+      }
+    }
+  }
+}
 
 function App() {
   return (
     <div className="App">
+      <Particles className='particles'
+        params={particleOptions}
+        />
       <Navigation />
       <Logo />
+      <Rank />
       <ImageLinkForm />
         { /* <Facerecognition />*/}
     </div>
