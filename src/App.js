6343d41abdf03e34171cb1b0f8d2a3890b7a8c25
OViki <v.ozsvart@gmail.com>
2023-07-07 22:59:18 +0200

    Remove particles, add input and button

--- src/App.js
+++ src/App.js
@@ -1,115 +1,40 @@
-import { useCallback } from 'react'
-import Particles from 'react-particles'
-import { loadFull } from 'tsparticles'
+import React, { Component } from 'react'
 import Navigation from './Components/Navigation/Navigation'
 import Logo from './Components/Logo/Logo'
 import ImageLinkForm from './Components/ImageLinkForm/ImageLinkForm'
 import Rank from './Components/Rank/Rank'
 import './App.css'
 
-const App = () => {
-    const particlesInit = useCallback(async (engine) => {
-        console.log(engine)
-        // you can initiate the tsParticles instance (engine) here, adding custom shapes or presets
-        // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
-        // starting from v2 you can add only the features you need reducing the bundle size
-        await loadFull(engine)
-    }, [])
+class App extends Component {
+    constructor() {
+        super()
+        this.state = {
+            input: '',
+        }
+    }
 
-    const particlesLoaded = useCallback(async (container) => {
-        console.log(container)
-    }, [])
+    onInputChange = (event) => {
+        console.log(event.target.value)
+    }
 
-    return (
-        <div className="App">
-            <Particles
-                className="particles"
-                id="tsparticles"
-                init={particlesInit}
-                loaded={particlesLoaded}
-                options={particlesOptions}
-            />
+    onButtonSubmit = () => {
+        console.log('click')
+    }
 
-            <Navigation />
-            <Logo />
-            <Rank />
-            <ImageLinkForm />
-            {/* <Facerecognition />*/}
-        </div>
-    )
+    render() {
+        return (
+            <div className="App">
+                <Navigation />
+                <Logo />
+                <Rank />
+                <ImageLinkForm
+                    onInputChange={this.onInputChange}
+                    onButtonSubmit={this.onButtonSubmit}
+                />
+                {/* <Facerecognition />*/}
+            </div>
+        )
+    }
 }
 
 export default App
-
-const particlesOptions = {
-    background: {
-        color: {
-            value: '#0d47a1',
-        },
-    },
-    fpsLimit: 120,
-    interactivity: {
-        events: {
-            onClick: {
-                enable: true,
-                mode: 'push',
-            },
-            onHover: {
-                enable: true,
-                mode: 'repulse',
-            },
-            resize: true,
-        },
-        modes: {
-            push: {
-                quantity: 4,
-            },
-            repulse: {
-                distance: 200,
-                duration: 0.4,
-            },
-        },
-    },
-    particles: {
-        color: {
-            value: '#ffffff',
-        },
-        links: {
-            color: '#ffffff',
-            distance: 150,
-            enable: true,
-            opacity: 0.5,
-            width: 1,
-        },
-        collisions: {
-            enable: true,
-        },
-        move: {
-            direction: 'none',
-            enable: true,
-            outModes: {
-                default: 'bounce',
-            },
-            random: false,
-            speed: 6,
-            straight: false,
-        },
-        number: {
-            density: {
-                enable: true,
-                area: 800,
-            },
-            value: 80,
-        },
-        opacity: {
-            value: 0.5,
-        },
-        shape: {
-            type: 'circle',
-        },
-        size: {
-            value: { min: 1, max: 5 },
-        },
-    },
-    detectRetina: true,
-}
